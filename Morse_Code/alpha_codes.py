47bfab2e0d1559c33e71ffba5d6414a4580128f4
Rebecca <205rcarter@gmail.com>
2020-03-24 11:56:31 -0700

    Adding Morse Code

---
+++ Morse_Code/alpha_codes.py
@@ -0,0 +1,3 @@
+def a():
+	dot()
+	dash()
---
+++ Morse_Code/morse_code.py
@@ -0,0 +1,158 @@
+import RPi.GPIO as GPIO
+import time
+
+GPIO.setmode(GPIO.BOARD)
+GPIO.setwarnings(False)
+
+ledPin = 12
+
+GPIO.setup(ledPin, GPIO.OUT)
+
+dot_length = .25
+dash_length = .75
+part = .25
+next_letter = .5
+space = 1.75
+
+def dot():
+	print("LED turning on")
+	GPIO.output(ledPin, GPIO.HIGH)
+	time.sleep(dot_length)
+	print("LED turning off")
+	GPIO.output(ledPin, GPIO.LOW)
+	time.sleep(part)
+
+def dash():
+	print("LED turning on")
+	GPIO.output(ledPin, GPIO.HIGH)
+	time.sleep(dash_length)
+	print("LED turning off")
+	GPIO.output(ledPin, GPIO.LOW)
+	time.sleep(part)
+
+def next():
+	print("LED turning off")
+	GPIO.output(ledPin, GPIO.LOW)
+	time.sleep(next_letter)
+
+def space():
+	print("Space")
+	GPIO.output(ledPin, GPIO.LOW)
+	time.sleep(space)
+
+def a():
+	dot()
+	dash()
+def b():
+	dash()
+	dot()
+	dot()
+	dot()
+def c():
+	dash()
+	dot()
+	dash()
+	dot()
+def d():
+	dash()
+	dot()
+	dot()
+def e():
+	dot()
+def f():
+	dot()
+	dot()
+	dash()
+	dot()
+
+def g():
+	dash()
+	dash()
+	dot()
+def h():
+	dot()
+	dot()
+	dot()
+	dot()
+def i():
+	dot()
+	dot()
+def j():
+	dot()
+	dash()
+	dash()
+	dash()
+def k():
+	dash()
+	dot()
+	dash()
+def l():
+	dot()
+	dash()
+	dot()
+	dot()
+def m():
+	dash()
+	dash()
+def n():
+	dash()
+	dot()
+def o():
+	dash()
+	dash()
+	dash()
+def p():
+	dot()
+	dash()
+	dash()
+	dot()
+def q():
+	dash()
+	dash()
+	dot()
+	dash()
+def r():
+	dot()
+	dash()
+	dot()
+def s():
+	dot()
+	dot()
+	dot()
+def t():
+	dash()
+def u():
+	dot()
+	dot()
+	dash()
+def v():
+	dot()
+	dot()
+	dot()
+	dash()
+def w():
+	dot()
+	dash()
+	dash()
+def x():
+	dash()
+	dot()
+	dot()
+	dash()
+def y():
+	dash()
+	dot()
+	dash()
+	dash()
+def z():
+	dash()
+	dash()
+	dot()
+	dot()
+
+a()
+next()
+b()
+next()
+c()
+next()
